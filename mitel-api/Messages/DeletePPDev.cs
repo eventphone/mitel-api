61aab31e4c0615b678f205062fde2eb6351eeee1
Bianco Veigel <devel@zivillian.de>
2018-03-27 22:31:53 +0200

    #10

---
+++ mitel-api/Messages/DeletePPDev.cs
@@ -0,0 +1,15 @@
+﻿using System.Xml.Serialization;
+
+namespace mitelapi.Messages
+{
+    public class DeletePPDev : BaseRequest
+    {
+        [XmlAttribute("ppn")]
+        public int Ppn { get; set; }
+    }
+
+    public class DeletePPDevResp : BaseResponse
+    {
+
+    }
+}
