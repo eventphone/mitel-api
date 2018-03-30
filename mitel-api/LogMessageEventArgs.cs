34a86323567c76bb7e1773c882e95ea0f1a01ff4
Bianco Veigel <devel@zivillian.de>
2018-03-30 12:21:25 +0200

    add missing file

---
+++ mitel-api/LogMessageEventArgs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace mitelapi
+{
+    public class LogMessageEventArgs : EventArgs
+    {
+        public LogMessageEventArgs(string message, MessageDirection direction)
+        {
+            Message = message;
+            Direction = direction;
+        }
+
+        public string Message { get; }
+
+        public MessageDirection Direction { get; }
+    }
+
+    public enum MessageDirection
+    {
+        In,
+        Out
+    }
+}
